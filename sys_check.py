2f97a66e9bce892af4b2200d40e3c7936cabc539
JamesH <yukidev@yukidev.com>
2024-04-16 14:35:23 -0600

    Moved syslog to base directory and added sys_check.py

---
+++ sys_check.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import os
+import shutil
+import socket
+import sys
+
+import psutil
+
+
+def check_reboot():
+    return os.path.exists("/run/reboot-required")
+
+
+def free_mem():
+    # placeholder for free memory check
+    return True
+
+
+def check_for_network():
+    """returns True if no network"""
+    try:
+        socket.gethostbyname("google.com")
+        return False
+    except:
+        return True
+
+
+def check_cpu_high():
+    # returns True if CPU is overburdened
+    return psutil.cpu_percent(1) > 75
+
+
+def check_disk_full(disk, min_gb, min_percent):
+    """returns True if root disk space is below 2 gb"""
+    du = shutil.disk_usage(disk)
+    # free space calc
+    percent_free = 100 * du.free / du.total
+    # convert to GB
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
+        return True
+    return False
+
+
+def check_root_full():
+    # WRAPPER FUNCITON - checks the root folder size
+    return check_disk_full(disk="/", min_gb=2, min_percent=10)
+
+
+def main():
+    """put all checks into a list of tuples to prevent code duplication"""
+    checks = [
+        (check_reboot, "Pending Reboot."),
+        (check_for_network, "No Network"),
+        (check_cpu_high, "cpu use over 75%"),
+        (check_root_full, "Root Partition has less than 2GB available"),
+    ]
+    everything_ok = True
+    for check, msg in checks:
+        # uses the first item in the tuple to run a check and if True return msg
+        if check():
+            print(msg)
+            everything_ok = False
+
+        # if multiple errors, only returns sys error once
+        if not everything_ok:
+            sys.exit(1)
+
+    print("Everything OK.")
+    sys.exit(0)  # doesn't return error msg
+
+
+main()
